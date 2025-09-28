92b75a966b7cce9606a2897fa515c7a751635c0b
Samuel <samuaelyeshambel@gmail.com>
2025-09-28 20:37:19 +0300

    Update config.php

--- pharmacy/config.php
+++ pharmacy/config.php
@@ -1,16 +1,12 @@
 <?php
-// Database configuration
-$host = "localhost";      // Server
-$user = "root";           // Default XAMPP MySQL user
-$pass = "";               // Leave empty if no password
-$db   = "pharmacy_system"; // Database name you created
+$host = getenv('DB_HOST');
+$user = getenv('DB_USER');
+$pass = getenv('DB_PASS');
+$db   = getenv('DB_NAME');
 
-// Create connection
 $conn = new mysqli($host, $user, $pass, $db);
 
-// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-// echo "Database connected successfully!";
 ?>
